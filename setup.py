2a7f51c81bcd4b2970a5478f6c0f70306d624a0d
Jayson Larose <jayson@interlaced.org>
2020-12-08 18:08:47 -0800

    * Yay, I'm uninstallable now!

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+import setuptools
 
 version = __import__("JaysTerm").__version__
-setup(
-	name = "JaysTerm",
-	version = version,
-	author = "Jayson Larose",
+setuptools.setup(
+	name         = "JaysTerm",
+	version      = version,
+	author       = "Jayson Larose",
 	author_email = "jayson@interlaced.org",
-	url = "https://github.com/jaysonlarose/JaysTerm",
-	description = "Jays' Terminal Handling Library",
+	url          = "https://github.com/jaysonlarose/JaysTerm",
+	description  = "Jays' Terminal Handling Library",
 	download_url = f"https://github.com/jaysonlarose/JaysTerm/releases/download/{version}/JaysTerm-{version}.tar.gz",
-	packages=['JaysTerm'],
-	requires=['jlib'],
+	packages     = ['JaysTerm'],
+	requires     = ['jlib'],
 )
